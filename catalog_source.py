f3c3d7060111d1e3a36e3bbbc26ade3ddd5b1db1
Ankush Thapa <ankushiitb@gmail.com>
2026-07-26 16:53:00 +0530

    Create catalog_source.py

---
+++ catalog_source.py
@@ -0,0 +1,19 @@
+CATALOG = [
+    {
+        "id": "ms-rachel",
+        "name": "Ms Rachel",
+        "videos": [
+            "https://www.youtube.com/watch?v=Y4UNW4BzZDU",
+            "https://www.youtube.com/watch?v=w264Mn-2MnQ",
+            "https://www.youtube.com/watch?v=8KtnrtHRiCg",
+        ],
+    },
+    {
+        "id": "super-simple-songs",
+        "name": "Super Simple Songs",
+        "videos": [
+            "https://www.youtube.com/watch?v=wtX9hMdLuew",
+            "https://www.youtube.com/watch?v=jZnBNf0GJyU",
+        ],
+    },
+]
